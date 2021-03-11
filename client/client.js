c7f7d4af0bb7f08a72d1fa16f85cb0c6675e6815
Abarcenas <abarcenas1111@gmail.com>
2021-03-11 03:37:39 -0800

    Added sockets/listening functionality to project. Working on simple chat system as well

--- client/client.js
+++ client/client.js
@@ -1,7 +1,30 @@
-const ws  = require('ws');
+<script>
+   var socket = io();
+   function setUsername() {
+      socket.emit('setUsername', document.getElementById('name').value);
+   };
+   var user;
+   socket.on('userExists', function(data) {
+      document.getElementById('error-container').innerHTML = data;
+   });
 
-const client = new ws('ws://localhost:3001');
+   socket.on('userSet', function(data) {
+      user = data.username;
+      document.body.innerHTML = '<input type = "text" id = "message">\
+      <button type = "button" name = "button" onclick = "sendMessage()">Send</button>\
+      <div id = "message-container"></div>';
+   });
 
-client.on('open', () => {
-  client.send('Hello');
-})
+   function sendMessage() {
+      var msg = document.getElementById('message').value;
+      if(msg) {
+         socket.emit('msg', {message: msg, user: user});
+      }
+   }
+
+   socket.on('newmsg', function(data) {
+      if(user) {
+         document.getElementById('message-container').innerHTML += '<div><b>' +
+            data.user + '</b>: ' + data.message + '</div>'
+      }
+   })
--- server.js
+++ server.js
@@ -1,31 +1,73 @@
 const express = require("express");
-var app = express();
-var http = require("http").createServer(app);
+var app = require('express')();
+var http = require("http").Server(app);
 var io = require("socket.io")(http);
+var path = require("path");
 var PORT = process.env.PORT || 3001;
 
 
 
 //https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css
 app.use(express.static(`${__dirname}/client`));
-
 // Testing to see if server can operate correctly
-app.get('/', function(req,res,next) {
-  res.send("Hello World");
-})
-
-// const wsServer = new ws.Server({noServer: true});
-// wsServer.on('connection', socket => {
-//   socket.on('message', message => console.log(message));
-// });
+app.get('/', function(req,res) {
+  res.sendFile(path.join(__dirname + '/client/index.html'));
+});
 
 
-//const server = app.listen(3001);
+var clients = 0;
+var roomno = 1;
+
+
+
+//Whenever someone connects this gets executed
+users = [];
+io.on('connection', function(socket) {
+   console.log('A user connected');
+   socket.on('setUsername', function(data) {
+      console.log("User "+data + " has connected!");
+      if(users.indexOf(data) > -1) {
+         socket.emit('userExists', data + ' username is taken! Try some other username.');
+      } else {
+         users.push(data);
+         socket.emit('userSet', {username: data});
+      }
+   });
+
+   socket.on('msg', function(data) {
+      //Send message to everyone
+      io.sockets.emit('newmsg', data);
+   })
+});
+
+//
+// users = [];
+//   io.on('connection', function(socket) {
+//      console.log('A user connected');
+//      //clients++;
+//
+//      socket.on('setUsername', function(data) {
+//         console.log("User "+data+" has connected!");
+//         if(users.indexOf(data) > -1) {
+//            socket.emit('userExists', data + ' username is taken! Try some other username.');
+//         } else {
+//            users.push(data);
+//            socket.emit('userSet', {username: data});
+//         }
+//      });
+//
+//      socket.on('msg', function(data) {
+//    //Send message to everyone
+//    io.sockets.emit('newmsg', data);
+// })
+//      //Whenever someone disconnects this piece of code executed
+//      socket.on('disconnect', function () {
+//         clients--;
+//         socket.broadcast.emit('newclientconnect', {description: clients + ' clients connected'});
+//         console.log('A user disconnected');
+//      });
+//   });
+
 http.listen(PORT, function() {
   console.log(`Server started on port: ${PORT}`);
 });
-// server.on('upgrade', (request, socket, head) =>{
-//   wsServer.handleUpgrade(request, socket, head, socket => {
-//     wsServer.emit('connection', socket, request);
-//   });
-// });
