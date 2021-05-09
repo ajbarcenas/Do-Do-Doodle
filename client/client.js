03f5ed1dab97f30f08236170b8191af4a23e8f4a
ajbarcenas <54606416+ajbarcenas@users.noreply.github.com>
2021-05-09 15:01:04 -0700

    Pushing finished game

--- client/client.js
+++ client/client.js
@@ -1,31 +1,93 @@
-
-var usersList = [];
 var myNick = "";
 
 var socket = io();
 
+// get canvas element and create context
+var canvas  = document.getElementById('drawing');
+var context = canvas.getContext('2d');
+var width   = 1200;
+var height  = 550;
 
+//set strokeWidth
+var strokeWidth = 5;
+$('#myslider').attr({min:1, max:25}).val(strokeWidth);
 
-scroll_bottom = function() {
-  if ($('#messages').length > 0) {
-    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+// set canvas to full browser width/height
+canvas.width = width;
+canvas.height = height;
+canvas.style.border = '5px solid black';
+
+function setMyName(){
+  myNick = document.getElementById("boxNick").value;
+  socket.emit("set nick", myNick);
+  document.getElementById("boxNick").disabled=true;
+  document.getElementById("buttonNick").hidden=true;
+  document.getElementById("boxMessage").disabled=false;
+  document.getElementById("buttonMessage").disabled=false;
+}
+
+function updateUserList(u){
+  let list = document.getElementById("ulist");
+  list.innerHTML = "";
+
+  for (var i = 0; i < u.length; i++ ) {
+      let {name, isArtist, isCorrect, isReady, isDisabled} = u[i];
+      let color = 'black';
+      let item = document.createElement("li");
+
+      if (isArtist) {
+        color = 'cyan';
+      } else if (isCorrect) {
+        color = 'green';
+      } else if (isDisabled) {
+        color = 'red';
+      }
+
+      item.innerHTML = `<span style="color: ${color}">${name}${isReady ? "&#10003" : ""}</span>`;
+
+      list.appendChild(item);
   }
 }
 
-socket.on("connect", function(e){
-  socket.emit("start");
-});
+var mouse = {
+  click: false,
+  pos: {},
+  pos_prev: {},
+};
 
-socket.on('nick', function(nick){
-  myNick=nick;
-});
+// register mouse event handlers
+canvas.onmousedown = function(e){
+  mouse.click = true;
+  mouse.pos_prev = {
+    // normalize mouse position to range 0.0 - 1.0
+    x: e.offsetX / canvas.width,
+    y: e.offsetY / canvas.height
+  };
+};
 
-$('form').submit(function(){
-  var temp = [myNick , $("#boxMessage").val()];
-  socket.emit("send chat message", temp);
-  $("#boxMessage").val("");
-  return false;
-});
+canvas.onmouseup = function(e){
+  mouse.click = false;
+};
+
+canvas.onmousemove = function(e) {
+    // check if the user is drawing
+    if (mouse.click) {
+      // normalize mouse position to range 0.0 - 1.0
+      mouse.pos = {
+        x: e.offsetX / canvas.width,
+        y: e.offsetY / canvas.height
+      };
+
+      // send line to to the server
+      socket.emit('draw_line', {
+        line: [ mouse.pos, mouse.pos_prev ],
+        color: document.getElementById("colorsMain").options[document.getElementById("colorsMain").selectedIndex].text,
+        strokeWidth: strokeWidth
+      });
+    }
+
+    mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
+};
 
 socket.on('chat message', function(msg){
   $("#messages").append("<li><b>"+msg[0]+":</b> "+msg[1]);
@@ -40,174 +102,103 @@ socket.on('users list', function(usersList){
     updateUserList(usersList);
 });
 
+//store all lines from the server
+socket.on('lines', function(lines) {
+  context.clearRect(0, 0,  canvas.width, canvas.height);
 
-function setMyName(){
-  myNick = document.getElementById("boxNick").value;
-  socket.emit("set nick", myNick);
-  document.getElementById("boxNick").disabled=true;
-  document.getElementById("buttonNick").hidden=true;
-  document.getElementById("boxMessage").disabled=false;
-  document.getElementById("buttonMessage").disabled=false;
-}
+  context.lineCap = 'round';
 
-function updateUserList(u){
-  var list = document.getElementById("ulist");
-  list.innerHTML = "";
+  //draw the currently stored lines
+  for (let i = 0; i < lines.length; i++) {
+    let {color, strokeWidth, line} = lines[i];
+    context.strokeStyle = color;
+    context.lineWidth = strokeWidth;
 
-  for (var i = 0; i < u.length; i++ ) {
-      var item = document.createElement("li");
-      item.innerHTML = u[i];
-      list.appendChild(item);
+    context.beginPath();
+    context.moveTo(Math.round(line[0].x * canvas.width), Math.round(line[0].y * canvas.height));
+    context.lineTo(Math.round(line[1].x * canvas.width), Math.round(line[1].y * canvas.height));
+    context.stroke();
+  }
+});
+
+socket.on('draw_line', function(data) {
+  let {color, strokeWidth, line} = data;
+  context.strokeStyle = color;
+  context.lineWidth = strokeWidth;
+  context.lineCap = 'round';
+
+  context.beginPath();
+  context.moveTo(Math.round(line[0].x * canvas.width), Math.round(line[0].y * canvas.height));
+  context.lineTo(Math.round(line[1].x * canvas.width), Math.round(line[1].y * canvas.height));
+  context.stroke();
+});
+
+socket.on('time', function(data) {
+  let timer = document.getElementById("timer");
+  if (data.done) {
+    timer.innerHTML = `<span style="color: black">Done!</span>`;
+  } else {
+    timer.innerHTML = `<span style="color: black">${data.timeLeft}s</span>`;
+  }
+});
+
+socket.on('word', function(word) {
+  alert(`Draw '${word}'`);
+});
+
+/*
+var choice = Math.floor(Math.random()*22);
+var answer = words[choice];
+var myLength = answer.length;
+
+let game = document.getElementById("game");
+let submitButton =document.getElementById("submitGuess");
+let newGameButton = document.getElementById("newGame");
+let result = document.getElementById("result");
+
+submitButton.addEventListener('click', function() {
+  let userGuess = document.getElementById("guessField").value;
+  console.log(answer);
+
+  if(userGuess == answer){
+    result.innerText = "Correct!";
+    game.style.backgroundColor = '#66ff75';
+  }
+  else{
+    result.innerText = "Incorrect!";
+    result.style.color = "black";
+    result.style.fontWeight = 'bold';
+    result.style.fontSize = '100';
+    game.style.backgroundColor = '#ff6666';
+  }
+});
+*/
+
+$('#myslider').on('input change',function(){
+  strokeWidth = parseInt($(this).val());
+});
+
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
   }
 }
 
+$('form').submit(function(){
+  var temp = [myNick , $("#boxMessage").val()];
+  socket.emit("send chat message", temp);
+  $("#boxMessage").val("");
+  return false;
+});
 
+$("#resetCanvas").click(function(){
+  socket.emit('clearit');
+});
 
-function clearit(){
-  socket.emit('clearit', true);
-  }
-document.addEventListener("DOMContentLoaded", function() {
-   var mouse = {
-      click: false,
-      move: false,
-      pos: {x:0, y:0},
-      pos_prev: false,
-      //color: 'red'
-   };
-   // get canvas element and create context
-   var canvas  = document.getElementById('drawing');
-   var context = canvas.getContext('2d');
-   var width   = 1200;
-   var height  = 550;
-   var socket  = io.connect();
+$("#readyButton").click(function() {
+  socket.emit('ready');
+});
 
-   // set canvas to full browser width/height
-   canvas.width = width;
-   canvas.height = height;
-   canvas.style.border = '5px solid black';
-
-   // register mouse event handlers
-   canvas.onmousedown = function(e){ 
-     mouse.click = true; 
-     //context.strokeStyle = getValueOfColorPicker;
-    };
-   canvas.onmouseup = function(e){ mouse.click = false; };
-
-   canvas.onmousemove = function(e) {
-      // normalize mouse position to range 0.0 - 1.0
-      mouse.pos.x = e.offsetX / width;
-      mouse.pos.y = e.offsetY / height;
-      mouse.move = true;
-   };
-
-   // draw line received from server
-	socket.on('draw_line', function (data) {
-      var line = data.line;
-      context.color = "red";
-      context.beginPath();
-      context.moveTo(line[0].x * width, line[0].y * height);
-      context.lineTo(line[1].x * width, line[1].y * height);
-      context.stroke();
-   });
-
-  socket.on('clearit', function(){
-    context.clearRect(0, 0, width, height);
-    console.log("client clearit");
-    });
-
-  socket.on('colorsMain', function(){
-    context.strokeStyle = getValueOfColorPicker;
-  });
-
-   // main loop, running every 25ms
-   function mainLoop() {
-      // check if the user is drawing
-      if (mouse.click && mouse.move && mouse.pos_prev) {
-         // send line to to the server
-         socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ] });
-         mouse.move = false;
-      }
-      mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
-
-      var getColorPickerByID = document.getElementById("colorsMain");
-      var getValueOfColorPicker = getColorPickerByID.options[getColorPickerByID.selectedIndex].text;
-      var canvas = document.getElementById('drawing');
-      var context = canvas.getContext('2d');
-      context.strokeStyle = getValueOfColorPicker;
-      setTimeout(mainLoop, 25);
-
-      $("#resetCanvas").click(function(){
-        var canvas= document.getElementById('drawing');
-       var ctx = canvas.getContext('2d');
-          ctx.clearRect(0, 0,  canvas.width, canvas.height);
-      });
-   }
-
-   //Put all this in its own function
-
-   var words = [
-    "America",
-    "Balloon",
-    "Biscuit",
-    "Blanket",
-    "Chicken",
-    "Chimney",
-    "Country",
-    "Cupcake",
-    "Curtain",
-    "Diamond",
-    "Eyebrow",
-    "Fireman",
-    "Florida",
-    "Germany",
-    "Harpoon",
-    "Husband",
-    "Morning",
-    "Octopus",
-    "Popcorn",
-    "Printer",
-    "Sandbox",
-    "Skyline",
-    "Spinach"];
-
-    var choice = Math.floor(Math.random()*22);
-    var answer = words[choice];
-    var myLength = answer.length;
-
-    let game = document.getElementById("game");
-    let submitButton =document.getElementById("submitGuess");
-    let newGameButton = document.getElementById("newGame");
-    let result = document.getElementById("result");
-
-    submitButton.addEventListener('click', function() {
-      let userGuess = document.getElementById("guessField").value;
-      console.log(answer);
-
-      if(userGuess == answer){
-        result.innerText = "Correct!";
-        game.style.backgroundColor = '#66ff75';
-      }
-      else{
-        result.innerText = "Incorrect!";
-        result.style.color = "black";
-        result.style.fontWeight = 'bold';
-        result.style.fontSize = '100';
-        game.style.backgroundColor = '#ff6666';
-      }
-    })
-          //var ctxx =canvas.getContext('2d');
-          context.lineCap='round';
-          var linewidth=5;
-          context.lineWidth=linewidth;
-
-          $myslider=$('#myslider');
-          $myslider.attr({min:1,max:25}).val(linewidth);
-
-          $myslider.on('input change',function(){
-          linewidth=context.lineWidth=parseInt($(this).val());
-          });
-
-
-
-   mainLoop();
+$("#submitGuess").click(function() {
+  socket.emit('guess', $("#guessField").val());
 });
--- server.js
+++ server.js
@@ -1,73 +1,161 @@
 const express = require("express")
-var app = require('express')();
+var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var path = require("path");
-var ip = require("ip");
 var PORT = process.env.PORT || 3000;
 
-
-var clients = [];
-var increment = 1;
+var clients = {};
 var line_history = [];
+var MAX_GUESSES = 3;
+
 var words = [
- "America",
- "Balloon",
- "Biscuit",
- "Blanket",
- "Chicken",
- "Chimney",
- "Country",
- "Cupcake",
- "Curtain",
- "Diamond",
- "Eyebrow",
- "Fireman",
- "Florida",
- "Germany",
- "Harpoon",
- "Husband",
- "Morning",
- "Octopus",
- "Popcorn",
- "Printer",
- "Sandbox",
- "Skyline",
- "Spinach"];
+  "America",
+  "Balloon",
+  "Biscuit",
+  "Blanket",
+  "Chicken",
+  "Chimney",
+  "Country",
+  "Cupcake",
+  "Curtain",
+  "Diamond",
+  "Eyebrow",
+  "Fireman",
+  "Florida",
+  "Germany",
+  "Harpoon",
+  "Husband",
+  "Morning",
+  "Octopus",
+  "Popcorn",
+  "Printer",
+  "Sandbox",
+  "Skyline",
+  "Spinach"
+];
 
+var game = {
+  word: null,
+  artist: null,
+  time: 60,   //seconds
+  timer: null,
+  live: false,
+  reset: function() {
+    this.live = false;
 
-//https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css
+    line_history = [];
 
-app.use(express.static(`${__dirname}/client`));
-// Testing to see if server can operate correctly
-app.get('/', function(req,res) {
-  res.sendFile(path.join(__dirname + '/client/index.html'));
-});
+    let client_IDs = Object.keys(clients);
+    if (client_IDs.length > 0) {
+      //grab random person
+      this.artist = client_IDs[Math.floor(Math.random() * client_IDs.length)];
+      //grab random word
+      this.word = words[Math.floor(Math.random() * words.length)];
 
+      Object.keys(clients).forEach(key => {
+        clients[key].ready = false;
+        clients[key].correct = false;
+        clients[key].guesses = 0;
+      });
+
+      io.emit('lines', line_history);
+      io.emit('users list', getUsersList());
+    } else {
+      this.word = null;
+      this.artist = null;
+      this.timer = null;
+    }
+  },
+  stop: function() {
+    if (this.timer != null && this.live) {
+      clearInterval(this.timer);
+      io.emit('time', {timeLeft: 0, done: true});
+    }
+    this.reset();
+  },
+  start: function() {
+    this.live = true;
+    //give the artist their word
+    clients[this.artist].socket.emit("word", this.word);
+
+    let timeLeft = this.time;
+    let that = this;
+    this.timer = setInterval(function() {
+      if (--timeLeft < 0) {
+        that.stop();
+      } else {
+        io.emit('time', {timeLeft: timeLeft, done: false});
+      }
+    }, 1000);
+
+    //set everyone to not ready
+    Object.keys(clients).forEach(key => {
+      clients[key].ready = false;
+    });
+
+    io.emit('users list', getUsersList());
+  },
+}
 
 function getUsersList(){
-  var usersList = [];
-    for (var i = 0; i < clients.length; i++){
-      usersList[i] = clients[i].n;
-    }
+  let usersList = [];
+  let i = 0;
+
+  Object.keys(clients).forEach(key => {
+    usersList[i++] = {
+      name: clients[key].name,
+      isArtist: key == game.artist,
+      isReady: clients[key].ready,
+      isCorrect: clients[key].correct,
+      isDisabled: clients[key].guesses >= MAX_GUESSES && !clients[key].correct
+    };
+  });
+
   return usersList;
 }
 
+//EXPRESS.JS
+app.use(express.static(`${__dirname}/client`));
 
-
+app.get('/', function(req,res) {
+  res.sendFile(path.join(__dirname + '/client/index.html'));
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  clients.push(socket);
+http.listen(PORT, function() {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
 
-  socket.on('start', function(){
-    socket.emit('nick', "guest"+increment);
-    clients[clients.indexOf(socket)].n = "guest"+increment;
-    increment++;
-    io.emit('users list', getUsersList());
+//SOCKET.IO
+io.on('connection', function(socket) {
+  clients[socket.id] = {
+    socket: socket,
+    name: `guest${Object.keys(clients).length+1}`,
+    ready: false,
+    correct: false,
+    guesses: 0
+  };
+
+  if (game.artist == null) {  //server was empty
+    game.reset();
+  }
+
+  socket.on('disconnect', function() {
+    if (clients[socket.id] != null && clients[socket.id].name != null) {
+      io.emit('info', `${clients[socket.id].name} disconnected.`);
+
+      delete clients[socket.id];
+
+      if (socket.id == game.artist) {
+        game.stop();
+      }
+
+      io.emit('users list', getUsersList());
+    }
   });
 
   socket.on('send chat message', function(msg){
@@ -75,43 +163,70 @@ io.on('connection', function(socket){
   });
 
   socket.on('set nick', function(nick){
+    clients[socket.id].name = nick;
     io.emit('info', "New user: " + nick);
-    clients[clients.indexOf(socket)].n = nick;
     io.emit('users list', getUsersList());
   });
 
-
-  socket.on('disconnect', function() {
-    if( clients[clients.indexOf(socket)].n == null ){
+  socket.on('draw_line', function (data) {
+    if (game.live && socket.id == game.artist) {
+      // add received line to history
+      line_history.push(data);
+      //broadcast received line
+      io.emit('draw_line', data);
     }
-    else{
-      io.emit('info', "User " + clients[clients.indexOf(socket)].n + " disconnected.");
-    }
-    clients.splice(clients.indexOf(socket),1);//clientIndex, 1);
-    io.emit('users list', getUsersList());
-   });
-
-   // first send the history to the new client
-for (var i in line_history) {
-   socket.emit('draw_line', { line: line_history[i] } );
-}
-
-// add handler for message type "draw_line".
-socket.on('draw_line', function (data) {
-   // add received line to history
-   line_history.push(data.line);
-   // send line to all clients
-   io.emit('draw_line', { line: data.line });
-});
-
-socket.on('clearit', function(){
-  line_history = [];
-  io.emit('clearit', true);
   });
 
+  socket.on('clearit', function() {
+    if (game.live && socket.id == game.artist) {
+      line_history = [];
 
-});
+      io.emit('lines', line_history);
+    }
+  });
 
-http.listen(PORT, function() {
-  console.log(`Server started on port: ${PORT}`);
+  socket.on('ready', function() {
+    if (!game.live) {
+      clients[socket.id].ready = true;
+
+      let client_IDs = Object.keys(clients), count = 0;
+      client_IDs.forEach(key => {
+        if (clients[key].ready) {
+          count++;
+        }
+      });
+      if (client_IDs.length > 1 && count == client_IDs.length) {
+        game.start();
+      }
+
+      io.emit('users list', getUsersList());
+    }
+  });
+
+  socket.on('guess', function(guess) {
+    if (game.live && socket.id != game.artist) {
+      if (!clients[socket.id].correct && clients[socket.id].guesses < MAX_GUESSES && game.word.toLowerCase() == guess.toLowerCase()) {
+          console.log("here")
+          clients[socket.id].correct = true;
+      } else {
+        clients[socket.id].guesses++;
+      }
+
+      io.emit('users list', getUsersList());
+
+      //reset the game if everyone is correct
+      let client_IDs = Object.keys(clients), count = 0;
+      client_IDs.forEach(key => {
+        if (clients[key].correct || clients[key].guesses >= MAX_GUESSES) {
+          count++;
+        }
+      });
+      if (client_IDs.length > 1 && count == client_IDs.length-1) {
+        game.stop();
+      }
+    }
+  });
+
+  socket.emit('lines', line_history);
+  io.emit('users list', getUsersList());
 });
